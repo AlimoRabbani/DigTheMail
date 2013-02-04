082390321edf72ac5ada619ecee2fcbac778205f
Alimohammad Rabbani <a.rabbani@me.com>
2013-02-04 15:01:29 +0330

    License information added.

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ DigTheMail is free software: you can redistribute it and/or modify it under the 
 
 DigTheMail is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with iTunification.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License along with DigTheMail. If not, see <http://www.gnu.org/licenses/>.
 
 
 About:
--- constants.py
+++ constants.py
@@ -1,2 +1,14 @@
+#License Information:
+#--------------------------------------
+#
+#Copyright (C) 2013  Alimohammad Rabbani
+#
+#DigTheMail is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+#DigTheMail is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License along with DigTheMail. If not, see <http://www.gnu.org/licenses/>.
+
+
 MAX_IMAP_CONNECTIONS = 3
 ATTACHMENT_DOWNLOAD_DIRECTORY = '../DigTheMailDownloadDirectory'
